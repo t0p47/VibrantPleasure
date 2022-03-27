25a02ac5c9a8cea7d934cb078340e37c988016e2
t0p47 <t0p47@yandex.ru>
2022-03-27 23:19:38 +0300

    dev-master-upgrade

--- app/src/main/java/com/t0p47/vibrantpleasure/extension/ViewModelFactory.kt
+++ app/src/main/java/com/t0p47/vibrantpleasure/extension/ViewModelFactory.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>): ViewModelProvider.Factory{
-    override fun <T: ViewModel?> create(modelClass: Class<T>): T{
+    override fun <T: ViewModel> create(modelClass: Class<T>): T{
         val viewModelProvider = viewModels[modelClass] ?: throw IllegalArgumentException("Model class $modelClass not found")
         return viewModelProvider.get() as T
     }
